d18affce08ede35a2733960fef903bc3e911bf1e
bsilawko <mrufkazkrem@protonmail.com>
2023-11-28 19:14:27 +0100

    Player can specify position of played letter

--- scrabble.py
+++ scrabble.py
@@ -9,26 +9,27 @@ class Game:
         self.bag = Bag()
         self.hand = Hand(self.bag)
 
-    def start_game(self):
-        print("Board: \n")
+    def print_game(self):
+        print("\n Board: \n")
         self.board.print_board()
-        print(f"\n Your letters: {self.hand}")
+        print(f"\n Your letters: {self.hand} \n")
         # print(hand)
 
-    def place_letter(self):
-        given_number = input("Give the number of letter you want to place: ")
-        letter = self.hand.get_letter(int(given_number))
-        self.hand.remove_letter(int(given_number))
-        self.board.update_board(letter, 8, 8)
-        print("Board: \n")
-        self.board.print_board()
-        print(f"\n Your letters: {self.hand}")
+    def play_letter(self):
+        given_number = int(input("Give the number of letter you want to play: "))
+        print("Where you want to place your letter")
+        given_row = int(input("Number of row: "))
+        given_col = int(input("Number of col: "))
+        letter = self.hand.get_letter(given_number)
+        self.hand.remove_letter(given_number)
+        self.board.update_board(letter, given_row, given_col)
+        self.print_game()
 
 
 if __name__ == "__main__":
     game = Game()
-    game.start_game()
-    game.place_letter()
+    game.print_game()
+    game.play_letter()
 
 # print(bag.get_left())
 # for i in range(0, 20):
