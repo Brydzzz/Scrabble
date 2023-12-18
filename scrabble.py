191664c9847645d02d85add3eb9ae16f727e3aa7
bsilawko <mrufkazkrem@protonmail.com>
2023-12-18 21:40:00 +0100

    added validation for adjacent letters in place_letter, ask player  to give blank letter value, changed all inputs to prompts from rich

--- scrabble.py
+++ scrabble.py
@@ -3,7 +3,7 @@ from bag import Bag
 from hand import Hand
 import numpy as np
 from rich import print as rprint
-from rich.prompt import IntPrompt
+from rich.prompt import IntPrompt, Prompt
 
 
 class Game:
@@ -41,12 +41,23 @@ class Game:
         """
         is_field_empty = self.board.check_if_cell_empty(given_row, given_col)
         is_letter_not_empty = self.hand.get_letter(given_number) != "_"
-        inputs_correct = (is_field_empty, is_letter_not_empty)
+        is_adjacent_to_letter = self.board.check_if_letter_around(
+            given_row, given_col
+        ) or (given_row, given_col) == (8, 8)
+        inputs_correct = (
+            is_field_empty,
+            is_letter_not_empty,
+            is_adjacent_to_letter,
+        )
         messages = []
         if not is_field_empty:
             messages.append("Choosen field is not empty")
         if not is_letter_not_empty:
             messages.append("Cannot play empty letter")
+        if not is_adjacent_to_letter:
+            messages.append(
+                "Letter has to be adjacent to an already placed letter"
+            )
 
         return all(inputs_correct), "\n".join(messages)
 
@@ -60,10 +71,6 @@ class Game:
             choices=["1", "2", "3", "4", "5", "6", "7"],
         )
         print("Where do you want to place your letter?")
-        rprint(
-            "[italic]Note: First letter in game will always be placed "
-            "at [/italic](8,8)"
-        )
         possible_row_col = [
             "1",
             "2",
@@ -81,24 +88,35 @@ class Game:
             "14",
             "15",
         ]
-        given_row = IntPrompt.ask("Row number", choices=possible_row_col)
-        given_col = IntPrompt.ask("Column number", choices=possible_row_col)
+        if np.all(self.board._cells[1:, 1:] == "___"):
+            print("First letter in the game will be placed at (8,8)")
+            given_row = 8
+            given_col = 8
+        else:
+            given_row = IntPrompt.ask("Row number", choices=possible_row_col)
+            given_col = IntPrompt.ask(
+                "Column number", choices=possible_row_col
+            )
         inputs_correct, player_message = self.validate_place_letter_inputs(
             given_number, given_row, given_col
         )
         if inputs_correct:
-            if np.all(self.board._cells[1:, 1:] == "___"):
-                print("First letter in the game will be placed at (8,8)")
-                given_row = 8
-                given_col = 8
             letter = self.hand.get_letter(given_number)
+            if letter == "?":
+                alphabet = list(self.bag.inside.keys())
+                alphabet.remove("?")
+                blank_value = Prompt.ask(
+                    "What letter should blank be?",
+                    choices=alphabet,
+                )
+                letter = blank_value
             self.hand.remove_letter(given_number)
             self.board.update_board(letter, given_row, given_col)
             self.print_game()
         else:
             rprint("[bold red]\nERROR[/bold red]")
             print(player_message)
-            print("\nTry again but with correct input\n")
+            print("\nTry again, but with correct input\n")
             self.place_letter()
 
     def place_letter_round(self):
@@ -121,11 +139,16 @@ class Game:
         """
         Executes when player chooses exchange letters option in play_round()
         """
-        number_of_letters = int(
-            input("Enter how many letter you want to exchange: ")
+        correct_numbers = ["1", "2", "3", "4", "5", "6", "7"]
+        number_of_letters = IntPrompt.ask(
+            "Enter how many letter you want to exchange",
+            choices=correct_numbers,
         )
         choosen_letter_numbers = [
-            int(input("Enter number of a letter you want to exchange: "))
+            IntPrompt.ask(
+                "Enter number of a letter you want to exchange",
+                choices=correct_numbers,
+            )
             for _ in range(number_of_letters)
         ]
         for letter_number in choosen_letter_numbers:
