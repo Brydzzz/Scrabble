c15616b3aba104b8013446fafe484c79d8774b6a
bsilawko <mrufkazkrem@protonmail.com>
2024-01-04 23:24:27 +0100

    added Bag class tests

--- test_bag.py
+++ test_bag.py
@@ -1,5 +1,5 @@
 from bag import Bag
-from constants import LETTERS_BAG_COUNT, HAND_EMPTY_LETTER_SYMBOL
+from constants import LETTERS_BAG_COUNT, HAND_EMPTY_LETTER_SYMBOL, HAND_SIZE
 
 
 def test_create_bag():
@@ -20,7 +20,7 @@ def test_draw_letter_typical(monkeypatch):
     assert drawn_letter == "A"
 
 
-def test_draw_letter_no_letters(monkeypatch):
+def test_draw_letter_empty_bag(monkeypatch):
     bag = Bag()
     bag._inside = {
         "A": 0,
@@ -77,3 +77,58 @@ def test_draw_letter_drawn_letter_not_available(monkeypatch):
     assert bag.inside["A"] == 0
     assert bag.inside["B"] == b_count - 1
     assert drawn_letter == "B"
+
+
+def test_exchange_letter(monkeypatch):
+    bag = Bag()
+    a_count_before = bag.inside["A"]
+
+    def draw_b(w):
+        return "B"
+
+    monkeypatch.setattr("random.choice", draw_b)
+    drawn_letter = bag.exchange_letter("A")
+    a_count_after = bag.inside["A"]
+    assert a_count_after == a_count_before + 1
+    assert drawn_letter == "B"
+
+
+def test_generate_hand():
+    bag = Bag()
+    hand = bag.generate_hand()
+    assert len(hand) == HAND_SIZE
+
+
+def test_print_bag(capsys):
+    bag = Bag()
+    bag.print_bag()
+    captured = capsys.readouterr()
+    all_outputs = captured.out.strip().split("\n")
+    first_output = all_outputs[0]
+    last_output = all_outputs[-1]
+    assert first_output == "There are 9 'A' left."
+    assert last_output == "Used letters: []"
+
+
+def test_check_letter_availability_true():
+    bag = Bag()
+    result = bag.check_letter_availability("A")
+    assert result is True
+
+
+def test_check_letter_availability_false():
+    bag = Bag()
+    bag._inside["A"] = 0
+    result = bag.check_letter_availability("A")
+    assert result is False
+
+
+def test_get_left_full_bag():
+    bag = Bag()
+    assert bag.get_left() == 100
+
+
+def test_get_left():
+    bag = Bag()
+    bag._inside["A"] -= 3
+    assert bag.get_left() == 97
